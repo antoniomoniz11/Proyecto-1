ea51715dd70542de2127a8adc9e029f9308fea4e
antoniomoniz11 <134645147+antoniomoniz11@users.noreply.github.com>
2023-05-27 12:58:27 -0400

    Update Proyecto.java

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -10,64 +10,89 @@ import javax.swing.JOptionPane;
 import java.io.PrintWriter;
 import java.io.IOException;
 import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  *
  * @author Antonio
  */
-public class Proyecto {
+public class Proyecto{
+    private static Grafo miGrafo;
+    
+    
+        private static Grafo extraerGrafo(String filePath){
+        Grafo res = null;
+        Lista<Usuario> verticesList = new Lista();
+         try {
+            
+            File file = new File(filePath);
+            Scanner scanner = new Scanner(file);
 
-    private static Usuario[] obtenerContenido(String ruta) throws IOException {
-    Usuario[] usuarios = new Usuario[20];
-    int numUsuarios = 0;
-    
-    boolean seccionUsuarios = false;
-    boolean seccionRelaciones = false;
-    
-    try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
-        String linea;
-        while ((linea = br.readLine()) != null) {
-            if (linea.startsWith("Usuarios")) {
-                seccionUsuarios = true;
-                continue;
-            } else if (linea.startsWith("Relaciones")) {
-                seccionUsuarios = false;
-                seccionRelaciones = true;
-                continue;
+          
+            if(scanner.hasNextLine()) scanner.nextLine();
+         
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if(line.equals("Relaciones")) break;
+                String[] parts = line.split(", @");
+                for(int i = 0; i < parts.length; i++) parts[i] = parts[i].trim();
+                verticesList.insertarFinal(new Usuario(Integer.parseInt(parts[0]), parts[1]));
+            }
+            Usuario[] vertices = new Usuario[verticesList.size()];            
+            Nodo<Usuario> actual = verticesList.getPfirst(); 
+            for(int i = 0; i < verticesList.size(); i++) {vertices[i] = actual.getElement(); actual = actual.getPnext();}
+            res = new Grafo(vertices);
+            
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                for(int i = 0; i < parts.length; i++) parts[i] = parts[i].trim();
+                res.insertarAristaValor(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
             }
             
-            if (seccionUsuarios) {
-              
-                String[] datosUsuario = linea.split(",");
-                int id = Integer.parseInt(datosUsuario[0]);
-                String nickname = datosUsuario[1];
-                Usuario usuario = new Usuario(id, nickname);
-                usuarios[numUsuarios] = usuario;
-                numUsuarios++;
-            } else if (seccionRelaciones) {
-             
-                String[] datosRelaciones = linea.split(",");
-                int idUsuario = Integer.parseInt(datosRelaciones[0]);
-                Usuario usuario = usuarios[idUsuario];
-                
-                for (int i = 1; i < datosRelaciones.length; i++) {
-                    int idAmigo = Integer.parseInt(datosRelaciones[i]);
-                    usuario.agregarAmigo(idAmigo);
-                }
-            }
+            scanner.close();            
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
         }
+        
+       
+       return res;
     }
     
-    Usuario[] resultado = new Usuario[numUsuarios];
-    System.arraycopy(usuarios, 0, resultado, 0, numUsuarios);
+    private static void guardarGrafo(Grafo grafo, String ruta){
+        try{
+        FileWriter fileWriter = new FileWriter(ruta+".txt");
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        
+        printWriter.println("Usuarios");
+        Usuario[] usuarios = grafo.getUsuarios();
+        for(Usuario usuario : usuarios){
+            if(usuario == null) break;
+            printWriter.println(usuario.getId()+", @"+ usuario.getNickname());
+        }
+        printWriter.println("Relaciones");
+        Arista[] aristas = grafo.getAristas();
+        for(Arista arista : aristas){
+            printWriter.println(usuarios[arista.getOrigen()].getId()+", "+ usuarios[arista.getDestino()].getId() +", "+ arista.getPeso());
+        }
+        
+        printWriter.close();
+    }catch(IOException e){}
+    }
     
-    return resultado;
-}
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        miGrafo = extraerGrafo("grafoText.txt");
+        Usuario juanito = new Usuario(999, "Juanito123");
+        
+        miGrafo.agregarUsuario(juanito);
+        miGrafo.insertarAristaValor(miGrafo.getUsuarios()[2].getId(), juanito.getId(), 4);
+        guardarGrafo(miGrafo, "GrafoConJuanito");
+        System.out.println(miGrafo.toString());
+        miGrafo = extraerGrafo("GrafoConJuanito.txt");
+        System.out.println(miGrafo.toString());
+        
         JFileChooser fileChooser = new JFileChooser();
 
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos de texto", "txt");
@@ -76,24 +101,25 @@ public class Proyecto {
         int result = fileChooser.showOpenDialog(null);
 
         if (result == JFileChooser.APPROVE_OPTION) {
-
+           
             java.io.File selectedFile = fileChooser.getSelectedFile();
-    
+            
+
+            // Opciones para guardar o descartar el archivo
             int option = JOptionPane.showOptionDialog(null, "¿Deseas guardar los datos actuales antes de cargar un nuevo archivo?",
                     "Advertencia", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE, null,
                     new Object[]{"Guardar", "Descartar", "Cancelar"}, "Guardar");
 
             if (option == JOptionPane.YES_OPTION) {
                 
+                
                 String ruta = "archivos/texto.txt";
 
                 try {
-                 
-                    Usuario[] usuarios = obtenerContenido(ruta);
-
+                   
                     PrintWriter writer = new PrintWriter(new FileWriter(ruta));
 
-                    
+                   
                     writer.close();
 
 
