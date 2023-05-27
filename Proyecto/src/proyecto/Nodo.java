018d59cbb077a3e88439e2c2016332e780f97741
antoniomoniz11 <134645147+antoniomoniz11@users.noreply.github.com>
2023-05-27 13:00:50 -0400

    Update Nodo.java

--- Proyecto/src/proyecto/Nodo.java
+++ Proyecto/src/proyecto/Nodo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto;
+package proyecto1;
 
 /**
  *
@@ -11,15 +11,20 @@ package proyecto;
  */
 public class Nodo<E>
 {
-    private E valor;
+    private E element;
     private Nodo pnext;
     
-    public Nodo(E valor, Nodo pnext){
-        this.valor = valor;
+    public Nodo(E element, Nodo pnext){
+        this.element = element;
         this.pnext = pnext;
     }
     
-    public E getElement() {return valor;}
+    public E getElement() {return element;}
     public Nodo getPnext() {return pnext;}
     public void setPnext(Nodo pnext) {pnext = pnext;}
+    
+    public boolean equals(Nodo<E> otro){
+        return element == otro.element && ((pnext == null && otro.pnext == null) || pnext.equals(otro.pnext));
+    }
 }
+
